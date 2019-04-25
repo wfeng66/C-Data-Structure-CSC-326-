3df9c5bd7449da32202d7c945474ed221f65a989
wfeng66 <weicongf73@gmail.com>
2019-04-25 19:27:03 -0400

    modified:   Labs & HWs/Lab11/Debug/Lab11.exe 	modified:   Labs & HWs/Lab11/Debug/Lab11.ilk 	modified:   Labs & HWs/Lab11/Debug/Lab11.pdb 	modified:   Labs & HWs/Lab11/Lab11.sdf 	modified:   Labs & HWs/Lab11/Lab11.v12.suo 	modified:   Labs & HWs/Lab11/Lab11/Debug/Lab11.log 	modified:   Labs & HWs/Lab11/Lab11/Debug/Source.obj 	modified:   Labs & HWs/Lab11/Lab11/Debug/vc120.idb 	modified:   Labs & HWs/Lab11/Lab11/Debug/vc120.pdb 	modified:   Labs & HWs/Lab11/Lab11/Source.cpp

--- Labs & HWs/Lab11/Lab11/Source.cpp
+++ Labs & HWs/Lab11/Lab11/Source.cpp
@@ -8,14 +8,41 @@ template <class Type>
 class stackADT{
 public:
 	stackADT();
+	// Default constructor, do nothing
+	// no input, no output
 	stackADT(int);
+	// Constructor, invokes initializeStack
+	// no input, no output
 	~stackADT();
+	// Destructor, release item array
+	// no input, no output
 	void initializeStack(int);
+	// Initialize empty array, dynamically creates empty stack that includes specified number of elements, size is setted as the number of input, topitem is setted as 0
+	// input: integer of the number of elements 
+	// output: no
 	bool isEmptyStack();
+	// Determines if the stack is empty
+	// input: no
+	// output: if the stack is empty, return ture; otherwise, return false
 	bool isFullStack();
+	// Determines if the stack is full
+	// input: no
+	// output: if the stack is full, return ture; otherwise, return false
 	void push(Type);
+	// Add new element into stack
+	// input: the value of the element need to be add into stack
+	// precondition: the stack is not full
+	// output: no
 	Type top();
+	// Get the value of the top element in the stack
+	// input: no
+	// precondition: the stack is not empty
+	// output: the value of the top element
 	void pop();
+	// Remove the top element from the stack
+	// input: no
+	// output: no
+	// precondition: the stack is not empty
 private:
 	Type* item;
 	int size;
